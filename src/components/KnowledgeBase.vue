5c1f97a157ac6e57fbdab965387b159f05281189
neerajdask <iamneerajdas@gmail.com>
2021-03-19 10:09:31 +0530

    Initial commit

---
+++ src/components/KnowledgeBase.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h2>Select a Topic</h2>
+    <knowledge-grid></knowledge-grid>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['topics'],
+  emits: ['select-topic'],
+};
+</script>
